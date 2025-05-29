425d93f150df02a43447688baed925561715cab1
juliebrunetto <julie.brunetto@neat.eu>
2025-05-29 17:59:55 +0200

    feat(events): add map with leaflet

---
+++ components/Map/Map.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="map"/>
+</template>
+
+<script setup lang="ts">
+import leaflet from "leaflet";
+import 'leaflet/dist/leaflet.css';
+import { onMounted } from "vue";
+
+onMounted(() => {
+  const map = leaflet
+      .map("map")
+      .setView([48.8566, 2.3522], 19);
+
+  leaflet
+      .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", { maxZoom: 19 }).addTo(map);
+});
+
+</script>
+
+<style scoped>
+#map {
+  width: 50vw;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
-useHead({
-  title: 'Rechercher un événement',
-})
+useHead({ title: 'Rechercher un événement' })
 const { data: events } = await useFetch('/api/events')
 </script>
 
 <template>
-  <h1>Rechercher un événement</h1>
-  <ul>
-    <li v-for="event in events" :key="event.title">
-      {{ event.title }}
-    </li>
-  </ul>
+  <div>
+    <h1>Rechercher un événement</h1>
+    <ul>
+      <li v-for="event in events" :key="event.title">
+        {{ event.title }}
+      </li>
+    </ul>
+    <ClientOnly>
+      <Map/>
+    </ClientOnly>
+  </div>
 </template>
